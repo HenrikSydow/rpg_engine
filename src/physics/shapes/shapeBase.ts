ad3e992e15f865cc3cc359b0616cee1b696ed3fa
HenrikSydow <henrik.alexander.sydow@gmail.com>
2023-05-08 17:41:36 +0200

    adds documentation for physics

--- src/physics/shapes/shapeBase.ts
+++ src/physics/shapes/shapeBase.ts
@@ -1,9 +1,19 @@
 
+/**
+ * The base class for all shapes.
+ */
 export abstract class ShapeBase {
 
+    /** This shapes local x coordinate. */
     protected localX: number;
+    /** This shapes local y coordinate. */
     protected localY: number;
 
+    /**
+     * Creates a new shape with local coordinates.
+     * @param localX This shapes local x.
+     * @param localY This shapes local y.
+     */
     constructor(localX: number, localY: number) {
         this.localX = localX;
         this.localY = localY;
