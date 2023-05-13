6f8f9471c23f3e2d4944c79b87eb8eccadbdd8c3
HenrikSydow <henrik.alexander.sydow@gmail.com>
2023-05-13 16:08:58 +0200

    adds gameObjectFactory and constants

---
+++ src/gameObjects/gameObjectConstants.ts
@@ -0,0 +1,17 @@
+
+/**
+ * Contains constants to use with GameObjects.
+ */
+export namespace GameObjectConstants {
+
+    /**
+     * Contains the names / ids of GameObjects.
+     */
+    export enum GameObjectName {
+        /*
+        Example GameObject name / id:
+        ExampleObject = "testObject",
+        */
+    }
+
+}
