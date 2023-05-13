05f7daf1bd6b021e68958a8c203b117d7b5cd0b5
HenrikSydow <henrik.alexander.sydow@gmail.com>
2023-05-13 15:43:38 +0200

    adds documentation for hitboxfactory and constants

--- src/physics/index.ts
+++ src/physics/index.ts
@@ -4,3 +4,5 @@ export * from "./hitboxes/hitboxHandler";
 export * from "./shapes/circle";
 export * from "./shapes/rectangle";
 export * from "./shapes/shapeBase";
+export * from "./hitboxes/hitboxConstants";
+export * from "./hitboxes/hitboxFactory";
